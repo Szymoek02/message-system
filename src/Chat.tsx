6e2fbd4ee826aab0d3efed121f39b3bec61fcb23
Szymoek02 <stajszczykszymon@gmail.com>
2024-03-09 12:17:00 +0100

    messages grouping

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import './message.css'
 
 export default function Chat()
 {
@@ -7,7 +9,6 @@ export default function Chat()
         SendAndReceived = 2,
         Seen = 3
     }
-
     type Message = {
         id: number;
         value: string;
@@ -15,29 +16,61 @@ export default function Chat()
         flag: MessFlags;
     }
 
-    const messages: Message[] = [{id: 1, value: "First message", sendOn: "now", flag: MessFlags.Send},
-                                 {id: 2, value: "Second message", sendOn: "yesterday", flag: MessFlags.SendAndReceived},
-                                 {id: 3, value: "Third message", sendOn: "09-09-2000", flag: MessFlags.Seen},
-                                 {id: 4, value: "Fourth message", sendOn: "00-00-0000", flag: MessFlags.Incomming}]
+    const messages: Message[] = [{id: 1, value: "Send message", sendOn: "00-00-0000", flag: MessFlags.Send},
+                                 {id: 2, value: "Send message", sendOn: "00-00-0000", flag: MessFlags.Send},
+                                 {id: 3, value: "SendAndReceived message", sendOn: "00-00-0000", flag: MessFlags.SendAndReceived},
+                                 {id: 4, value: "Incomming message", sendOn: "00-00-0000", flag: MessFlags.Incomming},
+                                 {id: 5, value: "Incomming message", sendOn: "00-00-0000", flag: MessFlags.Incomming},
+                                 {id: 6, value: "Seen message", sendOn: "00-00-0000", flag: MessFlags.Seen},
+                                 {id: 7, value: "Seen message", sendOn: "00-00-0000", flag: MessFlags.Seen},
+                                 {id: 8, value: "Incomming message", sendOn: "00-00-0000", flag: MessFlags.Incomming}]
+    
+    function outgoing(m: Message): boolean
+    {
+        return (m.flag === MessFlags.Seen || m.flag === MessFlags.Send || m.flag === MessFlags.SendAndReceived)
+    }
 
+    function groupMessages(inputArray: Message[]) : Message[][]
+    {
+        return inputArray.reduce((result: Message[][], currentElement, index, array) => {
+            if (index === 0 || (currentElement.flag === MessFlags.Incomming && outgoing(array[index - 1])) || 
+                (currentElement.flag !== MessFlags.Incomming && !outgoing(array[index - 1])))
+                result.push([currentElement])
+            else
+                result[result.length - 1].push(currentElement);
+            
+            return result
+        }, [])
+    }
+
+    console.log(groupMessages(messages));
     return (
         <div className="w-4/5 h-full flex flex-col justify-between">
             <div className="w-5/5 bg-neutral-900 h-16">
 
             </div>
-            <div className="h-full bg-neutral-800">
-                <ul>
+            <div className="h-full bg-neutral-800 overflow-x-auto">
+            {groupMessages(messages).map((msGroup, index) => {
+                if(msGroup[0].flag === MessFlags.Incomming)
                 {
-                    messages.map((ms) => {
-                        return <li className="list-none" key={ms.id}>
-                            <div className="rounded-full bg-neutral-600 max-w-fit p-1 text-sm text-slate-300 ">{ms.value}</div>
-                        </li>
-                    })
+                    return <ul key={index} className="group-custom-received list-none flex flex-col items-start text-slate-300 mx-2 my-1">{
+                        msGroup.map(ms => {
+                            return <li key={ms.id} className=" bg-neutral-600 max-w-fit p-1 px-2 my-px text-sm">{ms.value}</li>
+                    })}</ul>
                 }
-                </ul>
+                else
+                {
+                    return <ul key={index} className="group-custom-send list-none flex flex-col items-end text-slate-300 mx-2 my-1">{
+                        msGroup.map(ms => {
+                            return <li key={ms.id} className=" bg-neutral-600 max-w-fit p-1 px-2 my-px text-sm">{ms.value}</li>
+                    })}</ul>
+                }
+                
+            })}
             </div>
             <div className="w-5/5 bg-neutral-900 h-12">
-                
+                <input type="text" className=""/>
+                <button onClick={() => {}}></button>
             </div>
         </div>
     )
